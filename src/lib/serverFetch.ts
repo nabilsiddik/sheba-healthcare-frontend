74a468d308b2b32c8778d4f6e7135734614fb6df
nabilsiddik <nabilsiddik90@gmail.com>
2025-11-16 07:58:46 +0600

    Imlemented search filter and use debounce hook done

---
+++ src/lib/serverFetch.ts
@@ -0,0 +1,31 @@
+import { getCookie } from "@/utils/tokenHandler"
+
+const NEXT_PUBLIC_SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL
+
+export const serverFetchHelper = async (endPoint: string, options: RequestInit) => {
+    const { headers, ...restOptions } = options
+    const accessToken = await getCookie('accessToken')
+
+    const response = await fetch(`${NEXT_PUBLIC_SERVER_URL}${endPoint}`, {
+        headers: {
+            ...headers,
+            Cookie: accessToken ? `accessToken=${accessToken}` : ''
+        },
+        ...restOptions
+    })
+
+    return response
+}
+
+export const serverFetch = {
+    get: async (endPoint: string, options: RequestInit) => serverFetchHelper(endPoint, {...options, method: 'GET'}),
+    
+    post: async (endPoint: string, options: RequestInit) => serverFetchHelper(endPoint, {...options, method: 'POST'}),
+
+    put: async (endPoint: string, options: RequestInit) => serverFetchHelper(endPoint, {...options, method: 'PUT'}),
+
+    patch: async (endPoint: string, options: RequestInit) => serverFetchHelper(endPoint, {...options, method: 'PATCH'}),
+
+    delete: async (endPoint: string, options: RequestInit) => serverFetchHelper(endPoint, {...options, method: 'DELETE'})
+
+}
